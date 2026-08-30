1c9b1efc71da9c19f5459c7ea3eaf610c268a036
jbccre <102985297+jbccre@users.noreply.github.com>
2026-08-30 15:48:44 -0500

    Update app.R

--- app.R
+++ app.R
@@ -3,10 +3,22 @@ library(ggplot2)
 library(plotly)
 library(shiny)
 library(DT)
+library(readr)
 
 con <- url("https://raw.githubusercontent.com/jbccre/football/main/github.Rdata")
 load(con)
 close(con)
+
+ole_miss <- read_csv("https://raw.githubusercontent.com/jbccre/football/main/qualtrics.csv") |>
+  select(Q31,Q50) |>
+  arrange(Q50) |>
+  mutate(Q31=factor(Q31,levels=Q31)) |>
+  ggplot(aes(x=Q31,y=Q50,label=Q50)) +
+  geom_bar(stat='identity',fill='#C8102E') +
+  coord_flip() +
+  geom_label(fill='#C8102E',color='white',size=7) +
+  labs(y='Predicted Ole Miss Points',x='') +
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))
 
 historical_plot <- ggplot(historicalprobs, aes(x = day, y = prob, color=bracketname, group=bracketname,text = label)) +
   geom_line() +
@@ -40,13 +52,18 @@ ui <- fluidPage(
   hr(),
   h3("The Sports"),
   p("View the current status of each team, as well as which teams have already gone over/under."),
-  DTOutput("teams_table")
+  DTOutput("teams_table"),
+  hr(),
+  h3("Ole Miss Tiebreaker Question"),
+  p("View how many points people think Ole Miss will score this season."),
+  plotOutput("ole_miss", height='800px')
 )
 
 server <- function(input,output,session) {
   output$historical_plot <- renderPlotly({ggplotly(historical_plot, tooltip = c('text'))})
   output$players_table <- renderDT({players_table})
   output$teams_table <- renderDT({teams_table})
+  output$ole_miss <- renderPlot({ole_miss})
 }
 
 shinyApp(ui, server)
